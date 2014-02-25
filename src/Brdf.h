800259534f49330d18afc9e7939139c84757d659
Tanguy <Tanguy@172.16.125.218>
2014-02-25 10:44:27 +0100

    Deplacement du phong

--- src/Brdf.h
+++ src/Brdf.h
@@ -15,10 +15,26 @@
 #include "vector.h"
 
 class Brdf {
-private:
+protected:
 	PVect m_kd;
+	Brdf();
 public:
-	Brdf(PVect color);
+	Brdf(PVect color):m_kd(color){};
+};
+
+
+
+
+class Lambert : public Brdf {
+public:
+	Lambert(PVect kd):Brdf(kd){};
+	PVect ModeleLambert(Image *img,PVect vo,Source source,Rayon rayon);
+};
+
+class Phong : public Brdf{
+public :
+	PVect ModelePhong(Source source,Sphere sphere,PVect Ks,float n,Rayon rayon);
+	PVect CalcModelePhong(PVect puissance,PVect Kd,PVect Ks,float n,float alpha,float teta);
 };
 
 #endif /* BRDF_H_ */
--- src/Image.cpp
+++ src/Image.cpp
@@ -78,70 +78,7 @@ int Image::getRezY()
 //---------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------
-//applique le modele de phong sur la sphere "sphere" au point de contact avec le rayon avec une source en i,j de l'image
-//avec un coefficient n de spécularité et Ks la composante couleur de la spécularité
-void Image::ModelePhong(Source source,Sphere sphere,PVect Ks,float n,Rayon rayon,int i,int j)
-{
-	//calcul du teta
-		PVect Ps = source.getPosition();
-		//calcul point d'impact
-		PVect I=rayon.m_o+(rayon.m_t*rayon.m_v);
 
-		//calcul de la normale
-		PVect N=I-sphere.getCentre();
-		N.normalize();
-
-
-		//calcul du Vi
-		PVect Vi=Ps-I;
-		Vi.normalize();
-
-		//calcul du teta
-		float teta=N*Vi;
-
-		if (teta<0.0){
-			teta=0.0;
-		}
-
-		//calcul alpha
-		PVect H=Vi-rayon.m_v;
-		H.normalize();
-		float alpha=H*N;
-
-		if (alpha<0.0){
-			alpha=0.0;
-		}
-
-	this->setPixel(i,j,CalcModelePhong(source.getPuissance(),sphere.getColor(),Ks,n,alpha,teta));
-}
-//---------------------------------------------------------------------------
-//renvoie le calcul de phong avec la partie diffuse et la partie spéculaire
-PVect Image::CalcModelePhong(PVect puissance,PVect Kd,PVect Ks,float n,float alpha,float teta){
-
-		PVect partie_dif;
-		PVect partie_spec;
-		PVect P;
-
-		//calcul du cosalpha puissance n
-	    float value = pow(alpha, n);
-
-	    //calcul de la partie diffuse de Phong
-	    partie_dif.x = (1.0/M_PI)*Kd.x;
-	    partie_dif.y = (1.0/M_PI)*Kd.y;
-	    partie_dif.z = (1.0/M_PI)*Kd.z;
-
-	    //calcul de la partie speculaire de Phong
-	    partie_spec.x = ((n+2.0)/(2.0*M_PI))*Ks.x*value;
-	    partie_spec.y = ((n+2.0)/(2.0*M_PI))*Ks.y*value;
-	    partie_spec.z = ((n+2.0)/(2.0*M_PI))*Ks.z*value;
-
-	    //Application du modele de Phong
-	    P.x = puissance.x*(partie_dif.x + partie_spec.x)*teta;
-	    P.y = puissance.y*(partie_dif.y + partie_spec.y)*teta;
-	    P.z = puissance.z*(partie_dif.z + partie_spec.z)*teta;
-
-	    return P;
-}
 //---------------------------------------------------------------------------
 void Image::takePicture(float f, float dx, float dz, PVect p0, PVect origin, Scene myScene)
 {
